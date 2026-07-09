f90ed79b0400320d1b565c4de76063e8e7e2a3dc
bekaabera2-design <bekaabera2@gmail.com>
2026-07-08 23:36:27 -0700

    Update projectRoutes.js

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,11 +1,7 @@
 const router = require("express").Router();
 const projectController = require("../controllers/projectController");
 const { protect } = require("../middleware/authMiddleware");
-
-// Protect all project routes
 router.use(protect);
-
-// Project routes
 router.get('/', projectController.getAllProjects);
 router.get('/:id', projectController.getProjectById);
 router.post('/', projectController.createProject);
